9a608fb7a82bb10e99a8d43c537cd7456dbbb60f
71side-jin <kimhjin3@naver.com>
2025-06-05 13:39:20 +0900

    0605 생각해보니 로그인에 뒤로가기버튼이 있어야 함

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -15,14 +15,13 @@ export default function RootLayout() {
         <Stack.Screen
           name="firebase/login"
           options={{
-            headerShown: false,
+            headerTitle: "",
           }}
         />
         <Stack.Screen
           name="pattern/[id]"
           options={{
             headerShown: false,
-            //제목이 있음 좋겠는데 잘 모르겠어서 돌아가기버튼도 있겠다 걍 없앴는데 뒤로가기 버튼정도는 있는게 낫다싶으시면 밑에처럼 수정해주세요.
           }}
         />
         <Stack.Screen
